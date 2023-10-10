3f08d6325d1013f32828a07fb5dd2f37ca407d61
Jason <json.m.summers@gmail.com>
2023-10-10 13:57:43 +0100

    figuring out indices errors

--- src/features.py
+++ src/features.py
@@ -39,9 +39,13 @@ def match_2frame_features(keypoints1, descriptors1, keypoints2, descriptors2):
 
     # Filter matches based on valid keypoints
     valid_matches = []
+    valid_keypoint1 = []
+    valid_keypoint2 = []
+
     for match in matches:
-        if match.queryIdx < len(keypoints1) and match.trainIdx < len(keypoints2):
-            valid_matches.append(match)
+        valid_matches.append(match)
+        valid_keypoint1.append(keypoints1[match.queryIdx])
+        valid_keypoint2.append(keypoints2[match.trainIdx])
 
     # Check for invalid keypoints and log them
     invalid_matches = [match for match in matches if match not in valid_matches]
@@ -50,7 +54,7 @@ def match_2frame_features(keypoints1, descriptors1, keypoints2, descriptors2):
 
     ransac_threshold = 1.0  # Adjust this threshold as needed
     filtered_keypoints1, filtered_keypoints2, filtered_matches = apply_ransac(
-        keypoints1, keypoints2, valid_matches, ransac_threshold)
+        valid_keypoint1, valid_keypoint2, valid_matches, ransac_threshold)
 
     # Return the keypoints and filtered_matches
     return filtered_keypoints1, filtered_keypoints2, filtered_matches
@@ -58,15 +62,17 @@ def match_2frame_features(keypoints1, descriptors1, keypoints2, descriptors2):
 
 def apply_ransac(keypoints1, keypoints2, valid_matches, ransac_threshold):
     # Extract matching points
-    src_pts = np.float32([keypoints1[m.queryIdx].pt for m in valid_matches]).reshape(-1, 1, 2)
-    dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in valid_matches]).reshape(-1, 1, 2)
+    src_pts = np.float32([kp1.pt for kp1 in keypoints1]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2.pt for kp2 in keypoints2]).reshape(-1, 1, 2)
 
     # Use RANSAC to estimate the fundamental matrix
     fundamental_matrix, mask = cv2.findFundamentalMat(src_pts, dst_pts, cv2.FM_RANSAC, ransac_threshold, 0.99)
 
     # Apply the mask to filter out outliers
-    filtered_matches = [m for i, m in enumerate(valid_matches) if mask[i][0] == 1]
-    filtered_keypoints1 = [keypoints1[m.queryIdx] for m in filtered_matches]
-    filtered_keypoints2 = [keypoints2[m.trainIdx] for m in filtered_matches]
+    filtered_matches = [match for i, match in enumerate(valid_matches) if mask[i][0] == 1]
+
+    # Convert filtered keypoints to NumPy arrays
+    filtered_keypoints1 = np.array([kp1.pt for kp1 in keypoints1])
+    filtered_keypoints2 = np.array([kp2.pt for kp2 in keypoints2])
 
     return filtered_keypoints1, filtered_keypoints2, filtered_matches
