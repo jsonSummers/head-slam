848f29e07660e6a88d4618fe2b1200bead9b9522
Jason <json.m.summers@gmail.com>
2023-10-10 15:12:23 +0100

    rotation matrix can be calculated

--- src/features.py
+++ src/features.py
@@ -12,17 +12,7 @@ def extract_orb_features(image):
     return keypoints, descriptors
 
 
-def extract_and_match_features(image1, image2):
-    keypoints1, descriptors1, keypoints2, descriptors2 = extract_2frame_features(image1, image2)
-    filtered_keypoints1, filtered_keypoints2, filtered_matches = match_2frame_features(keypoints1, descriptors1,
-                                                                                       keypoints2, descriptors2)
-    return filtered_keypoints1, filtered_keypoints2, filtered_matches
-
-
 def extract_2frame_features(image1, image2):
-    # Initialize ORB detector
-    orb = cv2.ORB_create()
-
     # Detect ORB features and compute descriptors for both images
     keypoints1, descriptors1 = extract_orb_features(image1)
     keypoints2, descriptors2 = extract_orb_features(image2)
@@ -30,7 +20,66 @@ def extract_2frame_features(image1, image2):
     return keypoints1, descriptors1, keypoints2, descriptors2
 
 
-def match_2frame_features(keypoints1, descriptors1, keypoints2, descriptors2):
+def extract_and_match_features(image1, image2):
+    keypoints1, descriptors1, keypoints2, descriptors2 = extract_2frame_features(image1, image2)
+
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = matcher.match(descriptors1, descriptors2)
+
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    src_pts = np.float32([keypoints1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+
+    # You can adjust the RANSAC parameters (e.g., 3 for minimum number of points, 0.8 for the maximum reprojection
+    # error).
+    ransac_threshold = 3.0
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, ransac_threshold)
+    good_matches = [matches[i] for i in range(len(matches)) if mask[i] == 1]
+
+    return keypoints1, keypoints2, good_matches
+
+
+
+
+
+
+
+
+
+def match_2frame_features_old2(image1, image2):
+    keypoints1, descriptors1, keypoints2, descriptors2 = extract_2frame_features(image1, image2)
+
+    # Create a BFMatcher (Brute-Force Matcher) with default params
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors from image1 to image2
+    matches = bf.match(descriptors1, descriptors2)
+
+    # Filter matches based on valid keypoints
+    valid_matches = []
+    valid_keypoint1 = []
+    valid_keypoint2 = []
+
+    for match in matches:
+        valid_matches.append(match)
+        valid_keypoint1.append(keypoints1[match.queryIdx])
+        valid_keypoint2.append(keypoints2[match.trainIdx])
+
+    # Check for invalid keypoints and log them
+    invalid_matches = [match for match in matches if match not in valid_matches]
+    if invalid_matches:
+        print(f"Invalid matches: {len(invalid_matches)}")
+
+    ransac_threshold = 1.0  # Adjust this threshold as needed
+    filtered_keypoints1, filtered_keypoints2, filtered_matches = apply_ransac(
+        valid_keypoint1, valid_keypoint2, valid_matches, ransac_threshold)
+
+    # Return the keypoints and filtered_matches
+    return filtered_keypoints1, filtered_keypoints2, filtered_matches
+
+
+def match_2frame_features_old(keypoints1, descriptors1, keypoints2, descriptors2):
     # Create a BFMatcher (Brute-Force Matcher) with default params
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
--- src/test.py
+++ src/test.py
@@ -40,19 +40,25 @@ def test(loader, map, camera):
         # Extract and match features between the current frame and the previous one
         keypoints1, keypoints2, matches = extract_and_match_features(frame_sequence[-1], frame)
 
-        print_matches(keypoints1, keypoints2, matches)
+        #print_matches(keypoints1, keypoints2, matches)
 
         # Call triangulate_points with the initial inputs
         points3d = triangulate_points(keypoints1, keypoints2, matches, camera.camera_matrix,
                                       camera.rotation_matrix, camera.translation_vector,
                                       camera.rotation_matrix, camera.translation_vector)
-        print(points3d)
+
+        print(f'Number of filtered matches: {len(matches)}')
+        print(f'Number of triangulated points: {len(points3d)}')
+
         rotation_matrix, tvec = estimate_camera_pose(keypoints1, matches, camera, points3d)
-        break
+
+        print(rotation_matrix)
 
         for point3d, match in zip(points3d, matches):
             match.point3d = point3d  # Populate point3d attribute for the match
             map.add_point(point3d)
+
+
 
         # Update the camera's pose
         camera.update_pose(rotation_matrix, tvec)
